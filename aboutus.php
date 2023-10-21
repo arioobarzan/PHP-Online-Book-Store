8814f09e1a98968f66295cf7c02d19682b5c1eab
Arioo Barzan <arioobarzan@hotmail.com>
2023-10-21 18:32:10 +0330

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,10 @@
+﻿<?php
+require_once "main.php";
+echo"<br /><br /><p align='center' dir='RTL' style='text-align:center;direction:
+rtl;unicode-bidi:embed'><b>
+<span lang='FA' style='font-size: 26.0pt; font-family: B Nazanin; color: #F79646'>
+
+</span></b></p>
+";
+require_once "footer.php";
+?>

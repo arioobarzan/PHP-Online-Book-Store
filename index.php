caf6b678108e50e90c15173d82d68a52f0ca6814
Arioo Barzan <arioobarzan@hotmail.com>
2023-10-21 18:31:45 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once "main.php";
+echo"<p class='MsoNormal' align='center' dir='RTL' style='text-align:center;direction:
+rtl;unicode-bidi:embed'><br /><br />
+<span lang='FA' style='font-size: 18.0pt; font-family: B Nazanin'>به سايت
+فروشگاه آنلاين كتاب خوش آمديد.</span></p>
+<p class='MsoNormal' dir='RTL' style='text-align:right;direction:rtl;unicode-bidi:
+embed'><span lang='FA' style='font-size: 18.0pt; font-family: B Nazanin'>&nbsp;</span></p>
+<p class='MsoNormal' dir='RTL' style='text-align:right;direction:rtl;unicode-bidi:
+embed'><span lang='FA' style='font-size: 18.0pt; font-family: B Nazanin'></span></p>
+<p class='MsoNormal' dir='RTL' style='text-align:right;direction:rtl;unicode-bidi:
+embed'><span lang='FA' style='font-size: 18.0pt; font-family: B Nazanin'>ثبت نام
+در سايت رايگان است.</span></p>
+<p class='MsoNormal' dir='RTL' style='text-align:right;direction:rtl;unicode-bidi:
+embed'><span lang='FA' style='font-size: 18.0pt; font-family: B Nazanin'>در
+صورتي كه قصد سفارش دادن كتاب داريد بايد در سايت ثبت نام كنيد.</span></p>
+";
+require_once "footer.php";
+?>
